59fd8479268ef4f1286b0a0676fa7077402d6069
gokhan <gokhanasilturkk@gmail.com>
2025-05-07 19:15:35 +0300

    test

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ class Config:
     RELEVANCE_THRESHOLD = 0.6
     MAX_RESULTS = 10
     USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    STOPWORDS = {'nedir', 'nasıl', 'ne', 'için', 'ile', 've', 'bir', 'bu', 'şu', 'zaman', '?'}
+    STOPWORDS = {'nedir', 'nasıl', 'ne', 'için', 'ile', 've', 'bir', 'bu', 'şu', 'zaman'}
